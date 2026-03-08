eeb928766ecf96f3c5eeaef59d7a732207fd7770
sanjeevk021995 <sanjeev.kush95@gmail.com>
2026-03-08 08:17:04 +0530

    updated

--- src/main/java/com/devops/hello/HelloController.java
+++ src/main/java/com/devops/hello/HelloController.java
@@ -6,9 +6,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    // @GetMapping("/hello")
+    // public String hello() {
+    //     Thread.sleep(200);
+    //     return "Hello DevOps Monitoring!";
+    // }
+
+
     @GetMapping("/hello")
     public String hello() {
-        Thread.sleep(200);
-        return "Hello DevOps Monitoring!";
+        try {
+            Thread.sleep(200); // 200ms delay
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt(); // restore interrupt flag
+        }
+        return "Hello DevOps";
     }
+
 }
